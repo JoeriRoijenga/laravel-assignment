f2144897674869ed5d7934b9d4b4f0076c87c8fb
JoeriRoijenga <roijenga.joeri@gmail.com>
2021-01-10 13:43:37 +0100

    fix for migrations, added company in db

---
+++ database/migrations/2021_01_10_000000_create_companies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('path_to_logo');
+            $table->string('city');
+            $table->string('zip');
+            $table->string('street');
+            $table->string('housenumber');
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
